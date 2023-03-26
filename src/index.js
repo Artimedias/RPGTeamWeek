945e9ed9dc637c27264f75142f432e578e602ba6
Brett Roper <brettroper22@gmail.com>
2023-03-26 11:21:37 -0700

    saving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import Player from './js/player.js';
+import Invintory from './js/invintory.js';
 
-let Henry = new Player("Henry", 1, [5, 5, 5, 25])
+let inv = new Invintory()
+let Henry = new Player("Henry", 1, [5, 5, 5, 25], inv)
