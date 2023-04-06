f4b9f01b6ed36e385f4e87c0f294946285ac19a8
Brett Roper <brettroper22@gmail.com>
2023-04-05 18:04:49 -0700

    Fixed some typos

--- src/index.js
+++ src/index.js
@@ -164,7 +164,7 @@ let roomOneDoor = new SetPiece(1, 0, ["\n There is a door to the north, but it's
   }
 });
 
-let roomTwoComputer = new SetPiece(2, 0, ["\n There is a computer in sleep mode on one of the desks", "\n There is a computer in the room. Its screen reads ENNWWN in a forest green colour"], "computer", (item, textBox) => {
+let roomTwoComputer = new SetPiece(2, 0, ["\n There is a computer in sleep mode on one of the desks", "\n There is a computer in the room. Its screen reads WNNWWN in a forest green colour"], "computer", (item, textBox) => {
   if(item.name === "Flashdrive")
   {
     textBox.innerText += "\n Did you really just try to stick an unknown flashdrive into a computer? Are you insane?";    
@@ -269,7 +269,7 @@ let roomFiveMess = new SetPiece(5, 0, ["\n There is a massive mess of toys and t
   }
 });
 
-let roomSixGlass = new SetPiece(0, ["\n At the end of the table are three fine glasses. \nOne is full of a dark red liquid, the other a frothy brown. The third lies empty.", "\n All three glasses are full, and a door to the east has been opened"], "glasses", (item, textBox) => {
+let roomSixGlass = new SetPiece(6, 0, ["\n At the end of the table are three fine glasses. \nOne is full of a dark red liquid, the other a frothy brown. The third lies empty.", "\n All three glasses are full, and a door to the east has been opened"], "glasses", (item, textBox) => {
   if(item.name === "Whiskey")
   {
     textBox.innerText += "\n You pour the Whiskey into the last glass. \nAs it fills, classical music begins to play, and a hidden passage to the East reveals itself";
@@ -496,28 +496,29 @@ let roomFifteenGunman = new SetPiece(15, 0, ["\n A man dressed in a security gua
 
 
 //Defining rooms
-const roomOne = new Room(1, "\n You enter a dark and gloomy room that looks like a bunker. The only source of light is a flickering fluorescent lamp on the ceiling. The floor and walls are made of cold and rough concrete. \n", roomOneCons);
-const roomTwo = new Room(2, "\n This room looks modern and bright, with rows of desks and computers lining the walls and windows. \n There is a path to the South, North, and a path to the West", roomTwoCons);
-const roomThree = new Room(3, "\n You enter what looks like a hunters cabin, though one that couldn't of been occupied recently. \n You see a violent snowstorm raging outside the windows, which are boarded up with planks. You wonder how you got an office to a cabin in the middele of nowhere. \n You look around the room and see a fireplace, a sofa, a table and a closet. \n There is a path to the South", roomThreeCons);
-const roomFour = new Room(4, "\n You enter a dusty old bar. \nYou see some flickering neon lights, a piano with a full tip jar, and aging 80s decor. \n You hear some music playing from a jukebox in the corner. \n You look around and see some stools, tables, chairs and a counter. \n There is a path to the East and a path to the West", roomFourCons);
-const roomFive = new Room(5, "\n You walk into what looks like a childs room. \n There is a path to the North, East, and West", roomFiveCons);
-const roomSix = new Room(6, "\n You walk through the door and enter a large spacious banquet hall, \n adorned with beautiful decorations and elegant furniture. The room is lit by chandleirs hagning from the ceiling, casting a warm and welcoming glow throughout the hall. \n The centerpiece of the room is a long and ornate dining table, set with a delectable feast that seems to always move just out of reach.\n There is a path to the South", roomSixCons);
-const roomSeven = new Room(7, "\n As you enter the room you are transported to the heart of a massive forest. \n As you step inside, the room disolves around you and you find yourself standing in a clearing surrounded by towering trees and lush undergrowth. \n There is a path in all directions", roomSevenCons);
-const roomEight = new Room(8, "\n You enter what appears to be a gas station that has seen better days as much of the merchandise is secured behind metal bars.\nThe atomosphere in the room is slightly ominous, as the bars seem to suggest a sense of danger or insecurity. \n There is a path in all directions", roomEightCons);
+const roomOne = new Room(1, "\nYou enter a dark and gloomy room that looks like a bunker. The only source of light is a flickering fluorescent lamp on the ceiling. The floor and walls are made of cold and rough concrete.\n", roomOneCons);
+const roomTwo = new Room(2, "\nThis room looks modern and bright, with rows of desks and computers lining the walls and windows.\nThere is a path to the South, North, and a path to the West", roomTwoCons);
+const roomThree = new Room(3, "\nYou enter what looks like a hunter's cabin, though one that couldn't have been occupied recently.\nYou see a violent snowstorm raging outside the windows, which are boarded up with planks. \nYou look around the room and see a fireplace, a sofa, a table, and a closet to the West.\nThere is a path to the South", roomThreeCons);
+const roomFour = new Room(4, "\nYou enter a dusty old bar.\nYou see some flickering neon lights, a piano with a full tip jar, and aging '80s decor.\nYou hear some music playing from a jukebox in the corner.\nYou look around and see some stools, tables, chairs, and a counter.\nThere is a path to the East and a path to the West", roomFourCons);
+const roomFive = new Room(5, "\nYou walk into what looks like a child's room.\nThere is a path to the North, East, and West", roomFiveCons);
+const roomSix = new Room(6, "\nYou walk through the door and enter a large spacious banquet hall, adorned with beautiful decorations and elegant furniture.\nThe room is lit by chandeliers hanging from the ceiling, casting a warm and welcoming glow throughout the hall.\nThe centerpiece of the room is a long and ornate dining table, set with a delectable feast that seems to always move just out of reach.\nThere is a path to the South", roomSixCons);
+const roomSeven = new Room(7, "\nAs you enter the room, you are transported to the heart of a massive forest.\nAs you step inside, the room dissolves around you and you find yourself standing in a clearing surrounded by towering trees and lush undergrowth.\nThere is a path to the east and west", roomSevenCons);
+const roomEight = new Room(8, "\nYou enter what appears to be a gas station that has seen better days as much of the merchandise is secured behind metal bars.\nThe atmosphere in the room is slightly ominous, as the bars seem to suggest a sense of danger or insecurity.\nThere is a path in all directions", roomEightCons);
+
 const roomNine = new Room(9, "\n You walk through the door and enter a school hallway that stretches out to an extraordinary legnth, with rows of lockers lining each side of the passageway. \n The sounds of teenagers conversing and laughing can be heard in the distance yet no one is present.\n There is a mural on the wall. It shows a strange grid you have not seen before. \n[o][o][o][o][o][x][o][o]\n[o][o][o][o][o][x][o][o]\n[x][o][o][o][x][x][x][x]\n[x][x][x][o][x][x][o][o]\n[o][o][x][o][x][x][x][o]\n[o][o][x][x][x][x][x][o]\n[o][o][o][o][o][o][x][o] \n\n There is a path to the North and to the South", roomNineCons);
-const roomTen = new Room(10, "\n As you enter this room it opens up into an ancient temple with three large statues inside. The statues are a fox, a cat and a bunny. \n There is a path to the South and to the West", roomTenCons);
+const roomTen = new Room(10, "\n As you enter this room it opens up into an ancient temple with three large statues inside. The statues are a fox, a cat and a bunny. \n There is a path to the South, East and to the West", roomTenCons);
 const roomEleven = new Room(11, "\n You enter a pitch black room.", roomElevenCons);
 const roomTwelve = new Room(12, "\n As you pass across into the next room, you find yourself in a void with no way out. \n The only thing in the room are a table and mirror.", roomTwelveCons);
 const roomThirteen = new Room(13, "\n Once you enter the next room you find yourself facing a huge chessboard that covers the entire floor. You see four pieces on the chessboard: a black king on the eighth rank and the fifth file \n A black queen on the first rank and the fourth file and a white knight on the sixth rank and fifth file. \n There is a rook on the seventh rank and eighth file but it is not on the board. The rook hangs from a rope above the board with a sign that says Move Me. \n You realize this is a chess puzzle and you have to find the only move that will checkmate the black king to open the door to the next room.  \n There is a path to the North and to the West", roomThirteenCons);
 const roomFourteen = new Room(14, "\n This room is dimly lit with a single computer screen on a desk.\n There is a path to the South and a path to the North", roomFourteenCons);
 const roomFifteen = new Room(15, "\n Tthe next room it seems to be a paradise with a lush garden. \nA wooden gate appears to be the only path forward but in front of it is a maniacal man in a weird uniform holding a gun. \n The man snarls at you to get back in your prison or he will blow your brains out. \n There is a path to the South and a path to the North", roomFifteenCons);
 const roomSixteen = new Room(16, "\n You take the gate and walk forward, back to the normal world. \n\n\n\n You win!");
-const roomSeventeen = new Room(17, "\n You find yourself still in the forest, upon a clearing with a large boulder in the center.\n Now much deeper into the forest, you feel concerned,\n as if going the wrong direction could leave you lost forever", roomSeventeenCons);
-const roomEighteen = new Room(18, "\n You come upon a small pond deep within the woods, sourced from a stream to the North, dotted by lily pads and cattails.", roomEighteenCons);
-const roomNineteen = new Room(19, "\n The forest continues, the stream flowing from West to South crossing your way.", roomNineteenCons);
-const roomTwenty = new Room(20, "\n The stream's source is a small spring within a rocky outcropping between three large trees, the only natrual path to the West past the headwaters.", roomTwentyCons);
-const roomTwentyOne = new Room(21, "\n The forest continues unabated, its rows of towering pines obscuring any attempts to view far into the distance.", roomTwentyOneCons);
-const roomTwentyTwo = new Room(22, "\n After what felt like endless trees and shrubs, you happen upon a small wooden cabin within the heart of the forest, lit from within.", roomTwentyTwoCons);
+const roomSeventeen = new Room(17, "\n You find yourself still in the forest, upon a clearing with a large boulder in the center.\n Now much deeper into the forest, you feel concerned,\n as if going the wrong direction could leave you lost forever \n You can wander in any direction", roomSeventeenCons);
+const roomEighteen = new Room(18, "\n You come upon a small pond deep within the woods, sourced from a stream to the North, dotted by lily pads and cattails.\n You can wander in any direction", roomEighteenCons);
+const roomNineteen = new Room(19, "\n The forest continues, the stream flowing from West to South crossing your way.\n You can wander in any direction", roomNineteenCons);
+const roomTwenty = new Room(20, "\n The stream's source is a small spring within a rocky outcropping between three large trees.\n You can wander in any direction", roomTwentyCons);
+const roomTwentyOne = new Room(21, "\n The forest continues unabated, its rows of towering pines obscuring any attempts to view far into the distance.\n You can wander in any direction", roomTwentyOneCons);
+const roomTwentyTwo = new Room(22, "\n After what felt like endless trees and shrubs, you happen upon a small wooden cabin within the heart of the forest, lit from within.\n You can wander in any direction", roomTwentyTwoCons);
 
 
 
